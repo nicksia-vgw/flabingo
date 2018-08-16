d3b3984a264c83483a845bfebcceb0964209b69f
Nick Sia <nicholas.sia@vgw.co>
2018-08-16 22:31:40 +0800

    Add plugins

---
+++ Assets/Plugins/FasterDux/ActionMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unidux;
+
+namespace Plugins.FasterDux {
+	public interface IActionMapping<TState> {
+		Type ActionType();
+		Func<TState, IAction, TState> Reducer();
+	}
+
+	public class ActionMapping<TState, TActionType> : IActionMapping<TState> where TActionType : class {
+		private readonly Func<TState, TActionType, TState> _reducer;
+
+		public ActionMapping(Func<TState, TActionType, TState> reducer) {
+			_reducer = reducer;
+		}
+
+		public Type ActionType() => typeof(TActionType);
+		public Func<TState, IAction, TState> Reducer() => (state, action) => _reducer(state, action as TActionType);
+	}
+}
---
+++ Assets/Plugins/FasterDux/Reducer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unidux;
+
+namespace Plugins.FasterDux {
+    public class Reducer<TState> where TState : StateBase {
+        private readonly List<IActionMapping<TState>> _actionMap;
+
+        public Reducer(List<IActionMapping<TState>> actionMap) {
+            _actionMap = actionMap;
+        }
+
+        public TState Reduce(TState state, IAction action) {
+            return _actionMap
+                .Where(m => m.ActionType() == action.GetType())
+                .Aggregate(state, (s, m) => m.Reducer()(s, action));
+        }
+    }
+}
