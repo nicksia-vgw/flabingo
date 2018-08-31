ba2432930db9042ad0d5314f69c92824641b5a3f
Nick Sia <nicholas.sia@vgw.co>
2018-08-31 11:38:02 +0800

    Emoji!

---
+++ Assets/ChatHeadView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatHeadView : MonoBehaviour {
+    private Coroutine _coroutine;
+
+    public void ShowEmote(string emoteName) {
+        if (_coroutine != null) {
+            StopCoroutine(_coroutine);
+            GetEmoteObject().SetActive(false);
+        }
+        _coroutine = StartCoroutine(ShowEmoteAsync(emoteName));
+    }
+
+    private IEnumerator ShowEmoteAsync(string emoteName) {
+        Image image = GetEmoteImage();
+        image.sprite = Resources.Load<Sprite>($"emotes/{emoteName}");
+        GetEmoteObject().SetActive(true);
+        yield return new WaitForSeconds(1);
+        GetEmoteObject().SetActive(false);
+    }
+
+    private Image GetEmoteImage() {
+        return transform.GetChild(0).GetChild(0).GetComponent<Image>();
+    }
+
+    private GameObject GetEmoteObject() {
+        return transform.GetChild(0).gameObject;
+    }
+}
